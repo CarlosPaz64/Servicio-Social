2fe786f31f61c48b3b0c7f444d0a18311e643cac
CarlosPaz64 <134614533+CarlosPaz64@users.noreply.github.com>
2024-08-14 12:32:12 -0600

    Cambios a la aplicación

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
-express = require("express");
+const express = require("express");
+const path = require("path");
 
-app = use.express();
+const app = express();
+
+// Configuración del puerto
+const PORT = 3000;
+
+// Sirviendo archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Redirigir a index.html cuando se acceda a la raíz
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'principal', 'index.html'));
+});
+
+// Redirigir a index.html cuando se acceda a la carpeta del estatus
+app.get('/formularioEstatus/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'formularioEstatus', 'index.html'));
+  });
+  
+
+// Iniciando el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+});

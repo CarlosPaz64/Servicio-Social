1c718fceb292f93198b79bbd0fc209079f5e20d4
Antonyo Herrera <151698538+moma14@users.noreply.github.com>
2024-08-20 11:59:00 -0600

    error en el form nrepo
    Error al registrar la reposición: Error: Bind parameters must not contain undefined. To pass SQL NULL specify JS null
        at PromisePool.execute (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\mysql2\promise.js:374:22)
        at registrarRepos (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\controllers\formularioReposicion.js:10:37)
        at Layer.handle [as handle_request] (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\express\lib\router\layer.js:95:5)
        at next (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\express\lib\router\route.js:149:13)
        at Route.dispatch (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\express\lib\router\route.js:119:3)
        at Layer.handle [as handle_request] (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\express\lib\router\layer.js:95:5)
        at C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\express\lib\router\index.js:284:15
        at Function.process_params (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\express\lib\router\index.js:346:12)
        at next (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\express\lib\router\index.js:280:10)
        at Function.handle (C:\Users\Usuario2-Servicio\Documents\GitHub\Servicio-Social\node_modules\express\lib\router\index.js:175:3) {
      code: undefined,
      errno: undefined,
      sql: undefined,
      sqlState: undefined,
      sqlMessage: undefined
    }

--- controllers/formularioReposicion.js
+++ controllers/formularioReposicion.js
@@ -2,16 +2,17 @@ const pool = require('../database/database'); // Importa el pool de conexiones
 
 const registrarRepos = async (req, res) => {
     const { num_folio, id_unico, curp, fullname, estatus, comentario } = req.body;
+    console.log(req.body);
 
     try {
         // Inserta los datos en la base de datos
+        console.log("Datos a insertar: ", req.body);
         await pool.execute('INSERT INTO tramites_verificacion (num_folio, id_unico, curp, fullname, tramite, comentario) VALUES (?, ?, ?, ?, ?, ?)', [num_folio, id_unico, curp, fullname, estatus, comentario]);
-
         // Si el registro es exitoso, redirige a la vista deseada
         res.redirect('/');
     } catch (error) {
         // Maneja el error si ocurre
-        console.error('Error al registrar el estatus:', error);
+        console.error('Error al registrar la reposición:', error);
         res.status(500).send('Ocurrió un error al registrar el estatus.');
     }
 };
